2c09d17f8fde1882dd9fbc19be71605e7c0adcae
ricardocastilloisc <ricardocastilloisc@gmail.com>
2015-09-02 15:04:59 -0500

    Instlacion de laravel

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace PrimerProyecto\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PrimerProyecto\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
