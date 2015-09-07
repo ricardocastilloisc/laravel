968abd3d095215f55073cc4ab6355973ade83469
Ricardo Orlando Castillo Olivera <ricardoorlandocastilloolivera@Air-de-Ricardo.lan>
2015-09-07 10:10:24 -0500

    Rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,29 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+/*
+Pruebas de rutas
+Route::get('prueba', function () {
+    return 'hola desde routes.php';
+});
 
+$router->get('nombre/{nombre}', function($nombre)
+{
+	return "Mi nombre es ".$nombre;
+
+});
+
+$router->get('edad/{edad}', function($edad)
+{
+	return "mi edad es ".$edad;
+});
+
+$router->get('edad2/{edad?}', function($edad = 20)
+{
+	return "mi edad es ".$edad;
+
+});
+*/
 Route::get('/', function () {
     return view('welcome');
 });
